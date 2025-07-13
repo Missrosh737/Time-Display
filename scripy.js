6db2219a7005d5ca1fc4599b0c8a55f6eb94b3b4
Roshan Fatima737 <missrosh737@gmail.com>
2025-07-14 02:10:41 +0500

    Add files via upload

---
+++ scripy.js
@@ -0,0 +1,14 @@
+let myage=22
+let height=5
+let myclass=11
+
+   myage=(myage+6)
+   function myage (multiplyby,incrementBy){
+    let myage=16 (incrementBy)*multiplyby
+   }
+myage(7,4)
+
+
+  console.log( myage)
+ printallvar(777777777)
+ <script 
